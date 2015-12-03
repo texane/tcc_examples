f63f4381f4faeedb773148ced0e4cfb233b54bd2
texane <texane@gmail.com>
2015-12-03 20:24:00 +0100

    dext_exec5

--- common/dext.h
+++ common/dext.h
@@ -29,11 +29,23 @@ void dext_fini(void);
 int dext_open_c(dext_handle_t*, const char*, const dext_sym_t*);
 void dext_close(dext_handle_t*);
 
-#define dext_exec(__h) ((__h)->f())
-#define dext_exec1(__h, __a) ((__h)->f(__a))
-#define dext_exec2(__h, __a, __b) ((__h)->f(__a, __b))
-#define dext_exec3(__h, __a, __b, __c) ((__h)->f(__a, __b, __c))
-#define dext_exec4(__h, __a, __b, __c, __d) ((__h)->f(__a, __b, __c, __d))
+#define dext_exec(__h) \
+((__h)->f())
+
+#define dext_exec1(__h, __a) \
+((__h)->f(__a))
+
+#define dext_exec2(__h, __a, __b) \
+((__h)->f(__a, __b))
+
+#define dext_exec3(__h, __a, __b, __c) \
+((__h)->f(__a, __b, __c))
+
+#define dext_exec4(__h, __a, __b, __c, __d) \
+((__h)->f(__a, __b, __c, __d))
+
+#define dext_exec5(__h, __a, __b, __c, __d, __e) \
+((__h)->f(__a, __b, __c, __d, __e))
 
 
 #endif /* DEXT_H_INCLUDED */
