80a3a7ea11ff6d663d454edaffda95b21664ad4f
texane <texane@gmail.com>
2015-12-03 20:18:22 +0100

    add dext

---
+++ common/dext.c
@@ -0,0 +1,136 @@
+/* dance extension */
+/* user provided codes, typically used as computation kernels */
+
+
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include <sys/types.h>
+#include "dext.h"
+#include "libtcc.h"
+
+
+int dext_init(void)
+{
+  return 0;
+}
+
+
+void dext_fini(void)
+{
+}
+
+
+static void on_tcc_error(void* err, const char* msg)
+{
+  *(int*)err = -1;
+}
+
+
+int dext_open_c(dext_handle_t* dext, const char* s, const dext_sym_t* app_syms)
+{
+  const dext_sym_t default_syms[] =
+  {
+    /* TODO: dance specific symbols */
+
+    /* math symbols */
+    { "sin", "double sin(double);", sin },
+    { "cos", "double cos(double);", cos },
+    { "sqrt", "double sqrt(double);", sqrt },
+    { "fabs", "double fabs(double);", fabs },
+
+    DEXT_INVALID_SYM
+  };
+
+  size_t nsym;
+  size_t i;
+  size_t j;
+  size_t new_len;
+  char* new_s;
+  char* new_p;
+  int err = -1;
+
+  /* allocate and setup tcc state */
+
+  dext->tcc = tcc_new();
+  if (dext->tcc == NULL) goto on_error_0;
+
+  tcc_set_options(dext->tcc, "-static -nostdlib -nostdinc");
+  tcc_set_output_type(dext->tcc, TCC_OUTPUT_MEMORY);
+
+  /* prepend symbol decls to code */
+
+  new_len = 0;
+
+  for (i = 0; default_syms[i].name != NULL; ++i)
+    new_len += strlen(default_syms[i].decl);
+
+  nsym = i;
+
+  if (app_syms != NULL)
+  {
+    for (i = 0; app_syms[i].name != NULL; ++i)
+      new_len += strlen(app_syms[i].decl);
+    nsym += i;
+  }
+
+  new_len += strlen(s);
+
+  new_s = malloc(new_len + 1);
+  if (new_s == NULL) goto on_error_1;
+  
+  new_p = new_s;
+
+  for (i = 0; default_syms[i].name != NULL; ++i)
+  {
+    const dext_sym_t* const sym = &default_syms[i];
+    for (j = 0; sym->decl[j]; ++j, ++new_p) *new_p = sym->decl[j];
+    tcc_add_symbol(dext->tcc, sym->name, sym->addr);
+  }
+
+  if (app_syms != NULL)
+  {
+    for (i = 0; app_syms[i].name != NULL; ++i)
+    {
+      const dext_sym_t* const sym = &app_syms[i];
+      for (j = 0; sym->decl[j]; ++j, ++new_p) *new_p = sym->decl[j];
+      tcc_add_symbol(dext->tcc, sym->name, sym->addr);
+    }
+  }
+
+  strcpy(new_p, s);
+
+  /* compile and relocate the code */
+
+  err = 0;
+  tcc_set_error_func(dext->tcc, &err, on_tcc_error);
+  if ((tcc_compile_string(dext->tcc, new_s) == -1) || (err)) goto on_error_2;
+
+  if (tcc_relocate(dext->tcc, TCC_RELOCATE_AUTO) < 0)
+  {
+    err = -1;
+    goto on_error_2;
+  }
+
+  /* resolve entry point */
+
+  dext->f = tcc_get_symbol(dext->tcc, "main");
+  if (dext->f == NULL)
+  {
+    err = -1;
+    goto on_error_2;
+  }
+
+ on_error_2:
+  free(new_s);
+ on_error_1:
+  if (err) tcc_delete(dext->tcc);
+ on_error_0:
+  return err;
+}
+
+
+void dext_close(dext_handle_t* dext)
+{
+  tcc_delete(dext->tcc);
+}
---
+++ common/dext.h
@@ -0,0 +1,39 @@
+#ifndef DEXT_H_INCLUDED
+#define DEXT_H_INCLUDED
+
+
+#include <stdint.h>
+#include <stdlib.h>
+#include "libtcc.h"
+
+
+typedef struct dext_sym
+{
+  const char* name;
+  const char* decl;
+  void* addr;
+} dext_sym_t;
+
+#define DEXT_INVALID_SYM { .name = NULL, }
+
+
+typedef struct dext_handle
+{
+  TCCState* tcc;
+  void (*f)();
+} dext_handle_t;
+
+
+int dext_init(void);
+void dext_fini(void);
+int dext_open_c(dext_handle_t*, const char*, const dext_sym_t*);
+void dext_close(dext_handle_t*);
+
+#define dext_exec(__h) ((__h)->f())
+#define dext_exec1(__h, __a) ((__h)->f(__a))
+#define dext_exec2(__h, __a, __b) ((__h)->f(__a, __b))
+#define dext_exec3(__h, __a, __b, __c) ((__h)->f(__a, __b, __c))
+#define dext_exec4(__h, __a, __b, __c, __d) ((__h)->f(__a, __b, __c, __d))
+
+
+#endif /* DEXT_H_INCLUDED */
