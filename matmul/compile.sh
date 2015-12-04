3290f879e4e562811f8b03d36ea65d7422f99755
fabien.lementec <fabien.lementec@gmail.com>
2015-12-04 09:37:17 +0100

    compile.sh use common

--- matmul/compile.sh
+++ matmul/compile.sh
@@ -1,29 +1,8 @@
 #!/usr/bin/env bash
 
-if `false`; then
-CROSS_COMPILE=
-TCC_DIR=/usr/local
-TCC_LIB_DIR=$TCC_DIR/lib64
-fi
+ARCH=local
 
-if `false`; then
-CROSS_COMPILE=/segfs/linux/dance_sdk/toolchain/arm-buildroot-linux-uclibcgnueabi/bin/arm-buildroot-linux-uclibcgnueabi-
-TCC_DIR=/segfs/linux/dance_sdk/deps/arm-buildroot-linux-uclibcgnueabi
-TCC_LIB_DIR=$TCC_DIR/lib
-fi
-
-if `false`; then
-CROSS_COMPILE=/segfs/linux/dance_sdk/toolchain/i686-nptl-linux-gnu/bin/i686-nptl-linux-gnu-
-TCC_DIR=/segfs/linux/dance_sdk/deps/i686-nptl-linux-gnu
-TCC_LIB_DIR=$TCC_DIR/lib
-fi
-
-if `true`; then
-TCC_DIR=/segfs/linux/dance_sdk/deps/x86_64-rnice
-TCC_LIB_DIR=$TCC_DIR/lib64
-fi
-
-TCC_INC_DIR=$TCC_DIR/include
+source ../common/compile.sh
 
 $CROSS_COMPILE\gcc \
 -static \
