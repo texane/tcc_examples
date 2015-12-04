21e5bffd38f4f3976ab223206149be6e74605f59
fabien.lementec <fabien.lementec@gmail.com>
2015-12-04 08:44:46 +0100

    rm configure_arm.sh

--- install/configure_arm.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-./configure \
---cpu=armv7l \
---prefix=install_arm \
---cc=/segfs/linux/dance_sdk/toolchain/arm-buildroot-linux-uclibcgnueabi/bin/arm-buildroot-linux-uclibcgnueabi-gcc
-
